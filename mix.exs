a10285918e083fbac4e8bae0d1702371abde9f78
Oliver Switzer <oliver@geometer.io>
2022-06-15 11:51:56 -0400

    Update lc_seg.livemd to export graphs to VegaLite spec since VegaLite isn't rendering properly for some reason

--- mix.exs
+++ mix.exs
@@ -26,12 +26,13 @@ defmodule LcSeg.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:jason, "> 0.0.0"},
       {:statistex, "~> 1.0"},
       {:euclid, "~> 0.2.5", only: :test},
       {:floki, "~> 0.32.0", only: [:dev, :test]},
       {:uuid, "~> 1.1.8"},
-      {:kino, "~> 0.5.0"},
-      {:vega_lite, "~> 0.1.0", only: [:dev]},
+      {:kino, "~> 0.6.1"},
+      {:vega_lite, "~> 0.1.4", only: [:dev]},
       {:similarity, "~> 0.2.1"},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
